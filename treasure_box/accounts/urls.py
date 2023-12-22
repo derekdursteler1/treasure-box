120c95fb6662dc1920a8399f4719300aa621e0fc
Derek Dursteler <derekdursteler@Wrens-MacBook-Air.local>
2023-12-21 21:41:35 -0700

    Basic timeline is now showing media

--- treasure_box/accounts/urls.py
+++ treasure_box/accounts/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 from .views import SignUpView, profile
 from django.contrib.auth.views import LoginView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('signup/', SignUpView.as_view(), name='signup'),
     path('login/', LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('profile/', profile, name='profile'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
